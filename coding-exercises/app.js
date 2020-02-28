2640d00f25cecd9d819cada15703c8987250f3eb
LaishaCalalpa <lcalalpa@gmail.com>
2020-02-28 15:29:35 +0000

    add coding exercises

--- coding-exercises/app.js
+++ coding-exercises/app.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
-	/**
-	 * TODO : Create your 5 variables to get elements
-	 * based on the classes you created in the `index.html`.
-	 *
-	 * Define your variables below this comment.
-	 */
+	const city = document.querySelector('.city')
+	const temperature = document.querySelector('.temperature')
+	const degree = document.querySelector('.degree')
+	const scale = document.querySelector('.scale')
+	const summary = document.querySelector('.summary')
+	const icon = document.querySelector('.icon')
 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(position => {
@@ -12,21 +12,46 @@ window.addEventListener('load', () => {
 			const long = position.coords.longitude;
 			const lat = position.coords.latitude;
 
-			/* TODO: Continue your fetch request to set the DOM Elements for
-			 * temperature, description/summary, and timezone.
-			 * Make sure to include error handling.
-			 */
+			const apiKey = 'c9f9e43fde3609873063e38181a90d89'
+			const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${apiKey}/${lat},${long}`
 
-			/*TODO: Set the weather icon */
+			return fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					degree.innerText = `Temperature: ${Math.floor(data.currently.temperature)}`
+					summary.innerText = `Summary: ${data.currently.summary}`
+					city.innerText = `Location: ${data.timezone}`
+					scale.innerText = 'F'
 
-			/**TODO: Add an event listener that toggles between Fahrenheit and Celcius
-			 * when a user clicks on the current temperature section.
-			 */
+					setIcon(icon, data.currently.icon)
+					changeScale(data.currently.temperature)
+
+				})
+				.catch(error => {
+					console.error(`There was an error`);
+				});
+
 		});
 	}
 
-	/**
-	 * TODO: Code out the remainder of `setIcons`function.
-	 */
-	const setIcons = () => {};
+	function setIcon(iconID, icon) {
+		const skycons = new Skycons({ "color": "white" });
+		skycons.add(iconID, icon);
+		skycons.play();
+	};
+
+	function changeScale(temperature) {
+		scale.addEventListener('click', () => {
+			if (scale.innerText === 'F') {
+				scale.innerText = 'C'
+				temperature = ((temperature - 32) * 5 / 9).toFixed(2)
+				degree.innerText = temperature
+			}
+			else if (scale.innerText === 'C') {
+				scale.innerText = 'F'
+				temperature = (Math.floor((temperature * 9 / 5) + 32))
+				degree.innerText = temperature
+			}
+		})
+	}
 });
